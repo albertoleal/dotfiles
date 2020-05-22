85d6ee2a1c566223aa8fcd35665ed4d412a0ff90
Alberto Leal <albertonb@gmail.com>
2020-05-22 14:36:29 +0100

    Add ability to install bash_it

--- run.sh
+++ run.sh
@@ -10,6 +10,11 @@ log() {
   echo
 }
 
+install_bash_it() {
+  git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
+  ~/.bash_it/install.sh
+}
+
 install_ansible() {
   log "Installing/upgrading Homebrew"
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -17,6 +22,10 @@ install_ansible() {
 }
 
 log "Installing/Updating home..."
+
+if [[ ! -z "${BASH_IT}" ]] ; then
+  install_bash_it
+fi
 
 if ! which ansible-playbook > /dev/null 2>&1 ; then
   echo "ansible-playbook not found on \$PATH, installing"
