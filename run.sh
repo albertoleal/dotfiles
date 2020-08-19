7e4d4771c280188af2fc33e83c7b33fe6a42e544
Alberto Leal <albertonb@gmail.com>
2020-08-19 10:37:09 +0100

    Move functions to bin and update user config

--- run.sh
+++ run.sh
@@ -11,8 +11,8 @@ log() {
 }
 
 install_bash_it() {
-  git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
-  ~/.bash_it/install.sh
+  git clone --depth=1 https://github.com/Bash-it/bash-it.git "$HOME/.bash_it"
+  "$HOME/.bash_it/install.sh"
 }
 
 install_ansible() {
@@ -23,7 +23,7 @@ install_ansible() {
 
 log "Installing/Updating home..."
 
-if [[ ! -z "${BASH_IT}" ]] ; then
+if [[ ! -d "$HOME/.bash_it" ]] ; then
   install_bash_it
 fi
 
