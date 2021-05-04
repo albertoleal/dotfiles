26ee27cd572d301e5404cc6f449fdcc43e5052c0
Alberto Leal <albertonb@gmail.com>
2021-05-04 09:51:05 +0100

    Remove zoomus and bash_it

--- run.sh
+++ run.sh
@@ -10,11 +10,6 @@ log() {
   echo
 }
 
-install_bash_it() {
-  git clone --depth=1 https://github.com/Bash-it/bash-it.git "$HOME/.bash_it"
-  "$HOME/.bash_it/install.sh"
-}
-
 install_ansible() {
   log "Installing/upgrading Homebrew"
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -22,10 +17,6 @@ install_ansible() {
 }
 
 log "Installing/Updating home..."
-
-if [[ ! -d "$HOME/.bash_it" ]] ; then
-  install_bash_it
-fi
 
 if ! which ansible-playbook > /dev/null 2>&1 ; then
   echo "ansible-playbook not found on \$PATH, installing"
