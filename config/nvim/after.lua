a13f9dcba4bcd0b54513f9871c9c920270b42176
Alberto Leal <alberto.leal@shopify.com>
2021-10-31 09:24:23 +0000

    Add some mappings to nvim

--- config/nvim/after.lua
+++ config/nvim/after.lua
@@ -6,8 +6,11 @@ cmd([[colorscheme dracula]])
 -- " Remove trailing spaces on save
 cmd([[autocmd BufWritePre * :%s/\s\+$//e]])
 
+-- " Search under word
+map('n', '<Leader>rg', '<ESC>:FZFRg<Space>', { noremap = true, silent = false })
+map('n', '<Leader>rw', '<ESC>:FZFRg <C-R><C-W><enter>', { noremap = true, silent = false })
 
--- " Window Splitting and file navigation
+-- " Split window and file navigation
 map('n', ',hs', ':split<CR>', {silent = true})
 map('n', ',vs', ':vsplit<CR>', {silent = true})
 map('n', '\\', ':NERDTreeToggle<CR>', {noremap = true, silent = true})
@@ -16,6 +19,9 @@ map('n', '<leader><space>', ':Vipe<CR>', {silent = true})
 
 -- " Switch between test and production code
 map('n', '<Leader>.', ':A<CR>',{})
+
+-- " Open nvim help
+map( "n", ",h", [[<Cmd>lua require'telescope.builtin'.help_tags({results_title='Help Results'})<CR>]], { noremap = true, silent = true })
 
 -- " Common typos
 cmd(':command! WQ wq')
@@ -26,3 +32,4 @@ cmd(':command! Qall qall')
 cmd(':command! W w')
 cmd(':command! Q q')
 
+require'lspconfig'.solargraph.setup{}
